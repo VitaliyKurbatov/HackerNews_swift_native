4a23cc9441b6e156d9a572cc6be51765d910703e
Vitaliy <kurbatovvitaliy@yandex.ru>
2020-03-31 22:47:11 +0300

    Added Firebase/Database

--- NewsTestMotor/ViewController.swift
+++ NewsTestMotor/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+	
+	var ref: DatabaseReference!
+	let baseUrl = "https://hacker-news.firebaseio.com/"
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		ref = Database.database(url: baseUrl).reference()
+		download()
 	}
 
-
+	func download() {
+		ref.child("v0/topstories").observeSingleEvent(of: .value) { (snapshot) in
+			let storyIds = snapshot.value as? [Int]
+			if let firstStoryId = storyIds?.first {
+				self.ref.child("v0/item/\(firstStoryId)").observeSingleEvent(of: .value) { (snapshot) in
+					let story = snapshot.value as? [String: Any]
+					print(story)
+				}
+			}
+		}
+	}
 }
 
